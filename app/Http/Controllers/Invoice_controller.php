c1d18ceab1d1fc8a76136667e51b32b6d48b1388
khalidabdullah <khalidabdullah213@gmail.com>
2020-04-12 13:09:56 -0700

    add field total cost

--- app/Http/Controllers/Invoice_controller.php
+++ app/Http/Controllers/Invoice_controller.php
@@ -39,6 +39,7 @@ class Invoice_Controller extends Controller
 
         foreach ($mst_inv as $key => $val) {
             $data_inv[$key] = $val;
+            $data_inv[$key]->total_cost = DB::table('dtl_invoice')->max('inv_cost')->where('inv_seq', '=', $val->inv_seq);
             $data_inv[$key]->inv_detail = DB::table('dtl_invoice')
                 ->where('inv_seq', '=', $val->inv_seq)
                 ->get();
@@ -70,7 +71,8 @@ class Invoice_Controller extends Controller
         $mst_inv = DB::table('mst_invoice')
             ->where('inv_seq', $request->inv_seq)
             ->first();
-
+        
+        $mst_inv->total_cost = DB::table('dtl_invoice')->max('inv_cost')->where('inv_seq', $request->inv_seq);
         $mst_inv->user_print = $user->user_code;
         $mst_inv->do_detail = DB::table('dtl_invoice')
             ->where('inv_seq', $request->inv_seq)
